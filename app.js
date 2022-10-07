3e6ae7b9b206bb4ed6e007d342aa08f55b1052e2
Jhonatan Yaranga <jhoonatan_0912@hotmail.com>
2022-10-07 15:32:33 -0500

    some validations changes and add dark mode

--- app.js
+++ app.js
@@ -1,45 +1,67 @@
-console.clear()
-const input = document.getElementById('input')
-const btnSearch = document.getElementById('btn-buscar')
+$(document).ready(function () {
+  console.clear()
+  const input = document.getElementById('input')
+  const btnSearch = document.getElementById('btn-buscar')
+  const container = document.createElement('div');
+  const btnToggleDarkMode = document.getElementById("btn-toggle-dark")
+  const formContainer = document.querySelector(".form-container")
 
-btnSearch.addEventListener('click', (e => {
-  e.preventDefault();
+  btnSearch.addEventListener('click', (e => {
+    e.preventDefault();
 
-  const getData = async (idCharacter) => {
-    try {
-      idCharacter = input.value;
-      console.log(idCharacter);
-      let data = await fetch("https://rickandmortyapi.com/api/character/" + idCharacter)
-      // console.log(data.lenght);
-      let result = await data.json();
-      // console.log(result);
-      // input.value = "";
-
-
-      let article = document.createElement('article')
-      let imgCharacter = document.createElement('img')
-      let pName = document.createElement('p')
-      let pStatus = document.createElement('p')
-      let pSpecies = document.createElement('p')
-      let pGender = document.createElement('p')
+    const getData = async (idCharacter = "") => {
+      try {
+        idCharacter = input.value;
+        if (idCharacter == "") {
+          alert("Input is empty, please insert a number");
+        } else if (idCharacter > 826) {
+          alert("Introduced number is not disponible")
+        }
+        else {
+          let data = await fetch("https://rickandmortyapi.com/api/character/" + idCharacter)
+          let result = await data.json();
+          // console.log(result);
+          input.value = "";
 
 
-      imgCharacter.src = `${result.image}`;
-      pName.innerText = `Nombre: ${result.name}`;
-      pStatus.innerText = `Estado: ${result.status}`;
-      pSpecies.innerText = `Especie: ${result.species}`;
-      pGender.innerText = `Genero: ${result.gender}`;
-      article.append(imgCharacter, pName, pStatus, pSpecies, pGender)
-      document.body.append(article)
+          let article = document.createElement('article')
+          let imgCharacter = document.createElement('img')
+          let pName = document.createElement('p')
+          let pStatus = document.createElement('p')
+          let pSpecies = document.createElement('p')
+          let pGender = document.createElement('p')
 
-      article.classList.add("font-bold", "p-5", "bg-red-400", "w-60", "m-auto", "mt-5", "rounded-2xl")
-    } catch (error) {
-      console.log(error);
+
+          imgCharacter.src = `${result.image}`;
+          pName.innerText = `Nombre: ${result.name}`;
+          pStatus.innerText = `Estado: ${result.status}`;
+          pSpecies.innerText = `Especie: ${result.species}`;
+          pGender.innerText = `Genero: ${result.gender}`;
+          article.append(imgCharacter, pName, pStatus, pSpecies, pGender)
+          container.append(article)
+          container.classList.add('container', "grid", "grid-cols-2", "gap-2", "sm:grid-cols-3", "lg:grid-cols-4", "m-auto")
+          // document.body.append(article)
+          document.body.append(container)
+          article.classList.add("font-bold", "p-5", "bg-red-400", "w-60", "m-auto", "mt-5", "rounded-2xl");
+        }
+
+      } catch (error) {
+        console.log(error);
+      }
+
     }
+    getData()
+  }))
+  $("#btn-toggle-dark").click(function () {
+    document.body.classList.toggle("dark")
+    formContainer.classList.add("border-white")
+    input.classList.add("text-black")
+    if (document.body.classList.contains("dark")) {
+      btnToggleDarkMode.src = "./assets/img/sun.svg"
+    } else {
+      btnToggleDarkMode.src = "./assets/img/moon.svg"
+    }
+  });
 
-  }
-  getData()
-}))
-
-
+});
 
